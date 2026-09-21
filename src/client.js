5aec51dd4222ed17f0d6d88c2ec23da2604f6a08
Ivo Petkov <ivo@ivopetkov.com>
2026-09-21 14:50:55 +0300

    Add convertHTMLToImage() method.

--- src/client.js
+++ src/client.js
@@ -219,6 +219,12 @@ export class Client {
         await this.#downloadFile(r.result.file, target);
     }
 
+    async convertHTMLToImage(code, width, height, format, target) {
+        this.#checkTargetFilename(target);
+        const r = await this.#sendRequest('/v0/html-to-image', { text: code, width, height, format });
+        await this.#downloadFile(r.result.file, target);
+    }
+
     async takeWebsiteScreenshot(url, width, height, scale, format, target) {
         this.#checkTargetFilename(target);
         const r = await this.#sendRequest('/v0/website-screenshot', {
